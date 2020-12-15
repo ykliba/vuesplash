d66f472e7b0088dc1569babe8cf2c14167f08db5
ykliba <mediaboxing2@gmail.com>
2020-12-15 13:24:06 +0900

    vuerouter

--- web/routes/web.php
+++ web/routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// APIのURL以外のリクエストに対してはindexテンプレートを返す
+// 画面遷移はフロントエンドのVueRouterが制御する
+Route::get('/{any?}', fn() => view('index'))->where('any', '.+');
+
+
--- web/webpack.mix.js
+++ web/webpack.mix.js
@@ -11,7 +11,11 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix.browserSync({
+    proxy: '0.0.0.0:8081', // アプリの起動アドレス
+    open: false // ブラウザを自動で開かない
+  })
+  .js('resources/js/app.js', 'public/js')
+  .sass('resources/sass/app.scss', 'public/css')
+  .version()
+  
