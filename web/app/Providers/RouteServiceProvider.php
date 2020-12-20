f349f478281a400b26eb6d1ec8ae5e06bad312ef
ykliba <mediaboxing2@gmail.com>
2020-12-20 12:52:04 +0900

    ui

--- web/app/Providers/RouteServiceProvider.php
+++ web/app/Providers/RouteServiceProvider.php
@@ -64,7 +64,7 @@ class RouteServiceProvider extends ServiceProvider
     protected function mapApiRoutes()
     {
         Route::prefix('api')
-            ->middleware('web') // ★ 'api' → 'web' に変更
+            ->middleware('web') 
             ->namespace($this->namespace)
             ->group(base_path('routes/api.php'));
     }
--- web/routes/web.php
+++ web/routes/web.php
@@ -18,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/{any?}', fn() => view('index'))->where('any', '.+');
 
 
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
